b94bc1fd44ec03036fcb8eb396c0e336dd24fa1d
Rafael Myauti <rafael.myauti@sensedata.com.br>
2023-12-11 01:17:54 -0400

    Created post and get endpoints to posts

--- socialmedia/main.py
+++ socialmedia/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from socialmedia.models.posts import UserPost, UserPostIn
 
 app = FastAPI()
 
-@app.get("/health")
-async def health():
-    return {"message": "Ok!"}
+post_table = {}
+
+@app.post("/post", response_model=UserPost)
+async def create_post(data: UserPostIn):
+    res = data.dict()
+    last_record_id = len(post_table)
+    new_post = {**res, "id": last_record_id}
+    post_table[last_record_id] = new_post
+    return new_post
+
+@app.get("/post", response_model=list[UserPost])
+async def get_post():
+    return list(post_table.values())
